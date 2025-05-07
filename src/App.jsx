f89f7f86436531a8c849c4c858588e99cab87087
Min Phanith <@example.com>
2025-05-07 21:36:21 +0700

    refactor: update index.html title and remove unused SVG files; clean up index.css

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1 className="text-3xl font-bold underline text-center">
         Hello world!
       </h1>
+      <p className="text-xl font-medium text-center">We are the champion of E-day this year</p>
     </>
   )
 }
